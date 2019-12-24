d56a6a036afccc87b2ecb616f03c1f6f1d71c855
Hugo Page <hugopage0017@gmail.com>
2019-12-25 10:09:55 +1300

    5th commit

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from fetch_data import Fetch
 from web_server import Server
+from table import Table
+import click
 
-def main():
-    fetch = Fetch()
-    data = fetch.get_device_list()
+fetch = Fetch()
+data = fetch.get_device_list()
+
+
+@click.group()
+def cli():
+    pass
+
+@click.command()
+def serve():
     server = Server(data)
     server.start_server()
 
-main()
+@click.command()
+def showtable():
+    table = Table()
+    create_table = table.create_table(data)
+    print(create_table)
+
+cli.add_command(serve)
+cli.add_command(showtable)
+
+if __name__ == "__main__":
+    cli()
---
+++ table.py
@@ -0,0 +1,19 @@
+from tabulate import tabulate
+
+class Table:
+    def __init__(self):
+        pass
+
+    def create_table(self, devices):
+        full_data = []
+        for d in devices:
+            arr = []
+            arr.append(d['type'])
+            arr.append(d['family'])
+            arr.append(d['managementIpAddress'])
+            arr.append(d['macAddress'])
+            arr.append(d['serialNumber'])
+            arr.append(d['upTime'])
+            full_data.append(arr)
+        table = tabulate(full_data, headers=["Type", "Family", "IP Address", "MAC Address", "Serial Number", "Uptime"])
+        return table
